3e7cbd59374e9c2613021745275eb089947e75f3
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-06-13 10:16:35 +0530

    Update. Updated routers using MVC architecture.

--- routers/musicRouter.js
+++ routers/musicRouter.js
@@ -2,28 +2,10 @@ const express = require('express')
 const router = express.Router()
 const audioModel = require('../models/musicModel')
 const audioUpload = require('../middleware/audioUpload')
+const {addAAudio, getAllAudio} = require('../controllers/musicContoller')
 
-router.get('/', async (request,response)=>{
-    const audio = await audioModel.find()
-    response.status(201).json(audio)
-})
+router.route('/').get(getAllAudio).post(audioUpload.single('audioSource'),addAAudio)
 
-router.post('/',audioUpload.single('audioSource') ,async (request,response)=>{
-    const {title, artist} = request.body
-    const audioSource = request.file ? request.file.path : null
-    try{
-        const newAudio = new audioModel({
-            title,
-            artist,
-            audioSource
-        })
-        const audio = await newAudio.save()
-        response.status(200).json(audio)
-    }
-    catch(error){
-        response.status(500).json({message : error.message})
-    }
-})
 
 router.get('/', (request,response)=>{
 
